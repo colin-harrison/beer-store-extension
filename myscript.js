cd9805d5cba0133d8e1ad47702ab4880c0631839
Colin Harrison <coharrison25@gmail.com>
2018-05-01 02:03:51 -0400

    refactored ABV retreival process

--- myscript.js
+++ myscript.js
@@ -1,16 +1,16 @@
 // Grab Table Header Elements and Get Their Children Into an Array
 var parentNodes = [];
 grandParentNodes = document.getElementsByTagName('thead')
-for (var i = 0; i < grandParentNodes.length; i++) {
+for (let i = 0; i < grandParentNodes.length; i++) {
   parentNodes[i] = grandParentNodes[i].childNodes[0]
 }
 
 // Insert a new Price per drink header before the store availability header
 var headers = document.getElementsByTagName('th');
-for (var i = 0; i < parentNodes.length; i++) {
-  var newNode = document.createElement('th')
+for (let i = 0; i < parentNodes.length; i++) {
+  let newNode = document.createElement('th')
   newNode.innerHTML = 'Price Per 1.5 oz Alcohol'
-  var storeAvailabilityHeader = parentNodes[i].childNodes[2]
+  let storeAvailabilityHeader = parentNodes[i].childNodes[2]
   parentNodes[i].insertBefore(newNode, storeAvailabilityHeader)
 }
 
@@ -19,47 +19,48 @@ var sizes = []
 var volumes = []
 var regex = /\d+/g;
 var sizeStrings = document.getElementsByClassName("size");
-for (var i = 0; i < sizeStrings.length; i++) {
+for (let i = 0; i < sizeStrings.length; i++) {
   do {
     var sizeMatch = regex.exec(sizeStrings[i].innerText)
     if (sizeMatch) {
       sizes.push(sizeMatch[0])
-      var volMatch = regex.exec(sizeStrings[i].innerText)
+      let volMatch = regex.exec(sizeStrings[i].innerText)
       volumes.push(volMatch[0])
     }
   } while (sizeMatch)
 }
 
-console.log(sizes)
-console.log(volumes)
+//console.log(sizes)
+//console.log(volumes)
 
 // Insert cells in the Price per Drink Column
 var oddRows = document.getElementsByClassName("odd");
 var evenRows = document.getElementsByClassName("even");
-for (var i = 0; i < oddRows.length; i++) {
-  var oddCell = oddRows[i].insertCell(2);
+for (let i = 0; i < oddRows.length; i++) {
+  let oddCell = oddRows[i].insertCell(2);
   oddCell.innerHTML = "New cell"
 
   if (i < evenRows.length) {
-    var evenCell = evenRows[i].insertCell(2)
+    let evenCell = evenRows[i].insertCell(2)
     evenCell.innerHTML = "New cell"
   }
 }
 
-// Grab ABV from page
-// if (str.indexOf('%') != -1) it's the one
-// var ABVregex = /\d+\.\d/g                                       // should allow for any beer that is > 9.9 % ABV
-var ABVregex = /\d+\.\d/g
-var possibleABV = document.getElementsByTagName('dd');
-for (var i = 0; i < possibleABV.length; i++) {
-  if (possibleABV[i].innerText.indexOf('%') != -1) {
-      var ABVmatch = regex.exec(possibleABV[i].innerText)
-      console.log(ABVmatch)
+function grabABV() {
+  let possibleABV = document.getElementsByTagName('dd');
+  for (let i = 0; i < possibleABV.length; i++) {
+    if (possibleABV[i].innerText.indexOf('%') != -1) {
+        let ABVmatch = possibleABV[i].innerText.slice(0,-1)
+        return ABVmatch
+    }
   }
+  return null
 }
+
+// Grab ABV from page
+var ABV = grabABV()
 
 /*
 * Need to grab:
 *   price
-*   ABV
 */
